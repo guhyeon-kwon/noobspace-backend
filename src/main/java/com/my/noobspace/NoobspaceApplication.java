d9ba19cd367b22c70fdb4f0d63186fe6f3a1208b
ghkwon <ghkwon@goldenplanet.co.kir>
2023-07-17 01:09:52 +0900

    first commit

---
+++ src/main/java/com/my/noobspace/NoobspaceApplication.java
@@ -0,0 +1,13 @@
+package com.my.noobspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoobspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoobspaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my/noobspace/NoobspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.noobspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoobspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
